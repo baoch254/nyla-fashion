44a395d83d48cc84289e4cb1998cf872b13c2209
Bao Cao <baoch254@gmail.com>
2021-05-02 10:42:50 +0700

    Fix error with lint-staged

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -3,12 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import { appRoutes } from './app.routes';
+import { IconModule } from './core-ui';
+import {
+  MainLayoutComponent,
+  SidebarComponent,
+  TopbarComponent,
+} from './layouts';
+import { BreadcrumbModule } from 'primeng/breadcrumb';
+
+const PrimeNGModules = [BreadcrumbModule];
+
+const LayoutComponents = [
+  MainLayoutComponent,
+  TopbarComponent,
+  SidebarComponent,
+];
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LayoutComponents],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    BrowserAnimationsModule,
+    CommonModule,
+    IconModule,
+    PrimeNGModules,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ apps/admin/src/app/layouts/index.ts
@@ -0,0 +1,3 @@
+export * from './main-layout.component';
+export * from './topbar/topbar.component';
+export * from './sidebar/sidebar.component';
